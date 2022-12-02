fd80dcf28156c0a62ac31bd86ba4b23e4f4ae445
Daxx <64298070+Daxxboii@users.noreply.github.com>
2022-12-03 04:49:09 +0530

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port =  3000 ;
+
+app.get('/', (req, res) => {
+  res.send('Health Check')
+})
+
+app.get('/status', (req, res) => {
+    res.send('ok')
+  })
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
